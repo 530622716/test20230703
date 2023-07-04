eae02e3b74baa1bb361f52da6fef4ac63e010ef2
530622716 <530622716@qq.com>
2023-07-04 18:14:20 +0800

    2027-07-04 完成devices信息读取

---
+++ DeviceInfo.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+# @Time     : 2023/7/4 18:13
+# @Author   : vicky 
+# @Email    : 530622716@qq.com
+# @File     ：DeviceInfo.py
+
+
+import os
+import time
+
+class Projectorinfo():
+  #  def __init__(self):
+
+#获取设备的基础信息,如厂家，型号，系统版本
+    def devicesinfo(self):
+        deviceName=os.popen("adb shell getprop ro.product.model").read()
+        platformVersion=os.popen("").read()
+        producer=os.popen().read()
+        return "产品型号：%s %s,系统版本：Android %s "(
+        producer.replace("/n"," "),deviceName.replace("/n"," "),platformVersion.replace("/n"," ")
+        )
+
+#检测设备是否连接成功，连接成功返回成功，连接失败返回失败
+def check_devices(self):
+    try:
+        devicesInfo=os.popen("adb devices").read
+        if "device" in devicesInfo.split("\n")[1]:
+            print("="*20,"连接成功","="*20)
+            print(devicesInfo)
+        else:
+            print("="*20,"未连接上设备，请检测设备连接","="*20)
+    except Exception as e:
+        print("设备连接不成功，报错：",e)
